e31bba6525bc5f9988ae551a09c62c5cb2af4d22
Dereck2002 <jaramilloderek13@gmail.com>
2024-06-04 19:46:21 -0500

    fto_perfil

--- WsMunicipioIonic/ws_gad.php
+++ WsMunicipioIonic/ws_gad.php
@@ -7,6 +7,7 @@ header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Methods: PUT,GET,POST');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization, Accept, X-Requested-With, x-xsrf-token');
 header('Content-Type: application/json; charset=utf-8');
+require 'vendor/autoload.php';
 
 $respuesta = "";
 $post = json_decode(file_get_contents("php://input"), true);
