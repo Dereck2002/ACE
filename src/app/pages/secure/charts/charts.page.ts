1227fe6c07c93bf4b011041af0bf917d7651ba2c
AlexanderSTDev <steven8724@gmail.com>
2024-08-21 23:53:21 -0500

    Fe_dashboard

--- src/app/pages/secure/charts/charts.page.ts
+++ src/app/pages/secure/charts/charts.page.ts
@@ -51,7 +51,7 @@ export class ChartsPage implements OnInit {
           color: '#666',
           font: {
             family: 'Inter',
-            weight: '500'
+            weight: 'bold',
           }
         }
       },
@@ -204,35 +204,35 @@ export class ChartsPage implements OnInit {
       }
       return parseFloat(valor) || 0; // Convertir a número y manejar NaN
     };
-  
+
     // Calcular el costo de las materias primas
     const costoMateriasPrimas = this.materiasPrimas.reduce((total, materia) => total + limpiarNumero(materia.costo), 0);
-  
+
     // Calcular el costo de la mano de obra
     const totalManoDeObra = this.manoDeObraList.reduce((total, mano) => total + limpiarNumero(mano.costo), 0);
-  
+
     // Calcular el costo de los costos indirectos
     const totalCostosIndirectos = this.costosIndirectosList.reduce((total, costo) => total + limpiarNumero(costo.costo), 0);
-  
+
     // Calcular el costo de otros gastos
     const totalOtrosGastos = this.otrosGastoList.reduce((total, costo) => total + limpiarNumero(costo.costo), 0);
-  
+
     // Calcular el costo de producción
     this.costoProduccion = parseFloat((costoMateriasPrimas + totalManoDeObra + totalCostosIndirectos + totalOtrosGastos - 0.02).toFixed(2));
-  
+
     // Calcular el costo de fábrica
     const beneficio = parseFloat((this.costoProduccion * (this.margenBeneficio / 100)).toFixed(2));
     this.costoFabrica = parseFloat((this.costoProduccion + beneficio).toFixed(2));
-  
+
     // Calcular el costo de distribución
     const utilidadVendedor = parseFloat((this.costoFabrica * (this.utilidadv / 100)).toFixed(2));
     this.costoDistribucion = parseFloat((this.costoFabrica + utilidadVendedor).toFixed(2));
-  
+
     // Calcular el precio de venta al público (PVP)
     const utilidadComercial = parseFloat((this.costoDistribucion * (this.utilidadc / 100)).toFixed(2));
     const impuestosCalculados = parseFloat((this.costoDistribucion * (this.impuestos / 100)).toFixed(2));
     this.pvp = parseFloat((this.costoDistribucion + utilidadComercial + impuestosCalculados).toFixed(2));
-  
+
     console.log('Costo de materias primas:', costoMateriasPrimas);
     console.log('Total de mano de obra:', totalManoDeObra);
     console.log('Total de costos indirectos:', totalCostosIndirectos);
@@ -270,9 +270,9 @@ export class ChartsPage implements OnInit {
   }
 
   async guardarDatos() {
-    if (!this.txt_producto || !this.margenBeneficio || !this.impuestos || !this.costoProduccion || 
-        !this.costoFabrica || !this.costoDistribucion || !this.pvp || !this.materiasPrimas.length || 
-        !this.manoDeObraList.length || !this.costosIndirectosList.length || !this.otrosGastoList.length) {
+    if (!this.txt_producto || !this.margenBeneficio || !this.impuestos || !this.costoProduccion ||
+      !this.costoFabrica || !this.costoDistribucion || !this.pvp || !this.materiasPrimas.length ||
+      !this.manoDeObraList.length || !this.costosIndirectosList.length || !this.otrosGastoList.length) {
       this.authService.showToast('Por favor, completa todos los campos antes de guardar.');
       return;
     }
--- src/app/pages/secure/home/home.page.ts
+++ src/app/pages/secure/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { Chart, ChartTypeRegistry } from 'chart.js';
+
 
 @Component({
   selector: 'app-home',
@@ -11,6 +13,8 @@ export class HomePage implements OnInit {
   data: any[] = [];
   view: any[] = [700, 400];
   productos = [];
+  myChart: Chart;
+  myChart2: Chart<'pie'> | null = null;
 
   // options
   gradient = true;
@@ -85,6 +89,9 @@ export class HomePage implements OnInit {
     setTimeout(() => {
       this.content_loaded = true;
     }, 2000);
+
+    this.generarChart();
+    this.graficoPastel();
   }
 
   onSelect(data): void {
@@ -97,5 +104,89 @@ export class HomePage implements OnInit {
 
   onDeactivate(data): void {
     console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
+
+  generarChart() {
+    const canvas = document.getElementById('myChart') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+
+    this.myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+
+  graficoPastel() {
+    const canvas = document.getElementById('myChart2') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+
+    this.myChart2 = new Chart<'pie'>(ctx, {
+      type: 'pie', // Especifica el tipo de gráfico
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'], // Etiquetas de las secciones del pastel
+        datasets: [{
+          label: 'My Dataset',
+          data: [12, 19, 3, 5, 2, 3], // Datos para cada sección del pastel
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true, // Hace que el gráfico sea responsivo
+        plugins: {
+          legend: {
+            position: 'top', // Posición de la leyenda
+          },
+          tooltip: {
+            enabled: true, // Habilita o deshabilita los tooltips
+          },
+        }
+      }
+    });
   }
 }
